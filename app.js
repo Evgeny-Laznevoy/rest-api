4682f03b6820b02f18555fc3da9416f0f49e56bb
Evgeny-Laznevoy <laznevoy86@gmail.com>
2020-04-21 23:52:15 +0500

    implementation rest api

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
+
+let CONTACTS = [
+    {id: v4(), name: 'Evgen', value: '+7-982-554-60-60', marked: false}
+]
+
+app.use(express.json())
+
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json(CONTACTS)
+})
+
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: 'Контакт был удален'})
+  })
+  
+  // PUT
+  app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
+  })
+
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
